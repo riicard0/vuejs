489398d5c986fcf0c8887c9ae962bf8d3540445f
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-01-09 15:56:31 -0300

    :up: Diretivas #1

--- Curso (Tiago Matos - YouTube)/app/src/App.vue
+++ Curso (Tiago Matos - YouTube)/app/src/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <TheHeader></TheHeader>
+  <!-- Exemplo 1 = v-show -->
+  <TheHeader v-show="showHeader"/>
+
+  <!-- Exemplo 2 = Variáveis -->
+  <div v-show="showName">
+    Nome: {{ firstName }} 
+    <br>
+    Sobrenome: {{ lastName }}
+    <br>
+    Nome completo: {{ firstName + ' ' + lastName }}
+  </div>
+
+  <!-- Exemplo 3 = v-if -->
+  <div v-show="showIf">
+    <div v-if="accessLevel === 'admin'">Administrador</div>
+    <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+    <div v-else>Current User</div>
+  </div>
+
+  <!-- Exemplo 4 = v-for -->
+  <div>
+    <div 
+      v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+      class="todos-item"
+    >
+      {{ index+=1 }} - {{ obj.title }}
+    </div>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -13,17 +42,65 @@ export default {
   components: {
     HelloWorld,
     TheHeader
-  }
-}
+  },
+  data(){
+    return {
+      showHeader: false,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      showName: false,
+      showIf: false,
+      accessLevel: '',
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+          }
+        }
+      }
 </script>
 
 <style>
+.todos-item {
+  background: #000;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: #fff;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
