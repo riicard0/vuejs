51c9a4772f483d46cc6d706f1655f42e37167994
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-03-02 17:31:07 -0300

    :up: Vue Router #2

--- Curso (Tiago Matos - YouTube)/app/src/App.vue
+++ Curso (Tiago Matos - YouTube)/app/src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/about">About</RouterLink> |
+    <RouterLink to="/usuarios/10">Usuário</RouterLink> |
     <RouterLink :to="rotaDinamica">Serviços</RouterLink>
   </nav>
 
---
+++ Curso (Tiago Matos - YouTube)/app/src/views/UserEditView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ $route.params.id }}
+
+    Usuário <br>
+
+    <button @click="login()">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    // console.log(this.$route);
+    // console.log(this.$router);
+  },
+
+  methods: {
+    login() {
+      //ajax login
+      this.$router.push('/about');
+      console.log('CHEGOU');
+    }
+  }
+}
+</script>
