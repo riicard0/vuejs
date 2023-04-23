4a92f983635483cf42c7c6c739a6f5880eadf0f0
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-04-23 17:12:27 -0300

    :up: Curso 3 - Recapitulando #3

---
+++ Curso (Pablo Codes - YouTube)/vue-tutorial/src/components/ProjetoTutorial.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="mostraNome()">Clique aqui</button>
+    <button @dblclick="mostraNome()">Clique aqui</button>
+    <button>Clique aqui</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    //variable
+    const nome = ref("dom");
+
+    function mostraNome() {
+      alert('Olá ' + nome.value)
+    }
+
+    return {
+      nome,
+      mostraNome
+    }
+  }
+}
+</script>
