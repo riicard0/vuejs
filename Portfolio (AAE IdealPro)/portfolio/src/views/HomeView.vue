2a3eff45adc5a035c2058e3516108d1c1c9b6a1c
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-05-11 14:12:59 -0300

    :up: portfolio #2

--- Portfolio (AAE IdealPro)/portfolio/src/views/HomeView.vue
+++ Portfolio (AAE IdealPro)/portfolio/src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <v-app id="home">
     <NavBar />
     <v-container fluid>
+
+      <!-- home -->
       <div class="head">
         <v-row>
           <v-col cols="5">
@@ -25,28 +27,261 @@
           </v-col>
         </v-row>
       </div>
+
+      <!-- about -->
       <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="i2.png" max-height="300"></v-img>
+              </div>
+            </v-col>
 
-        PAREI AQUI: 12:04 <br />
-        YouTube (AAE IdeaPro): https://www.youtube.com/watch?v=7yd5Q6b-rss&list=WL&index=2
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About Me</h5>
+              <div style="width: 120px">
+                <v-slider v-model="slider2" color="yellow" track-color="yellow">
 
+                </v-slider>
+              </div>
+              <h4 class="mt-n4">I Am a Creative Web Design</h4>
+              <h4>And Developer</h4>
+              <p class="text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et at nemo ut ad, quo
+                corrupti ullam praesentium animi eius aliquid fugiat nihil voluptates nulla? Enim saepe provident
+                perspiciatis assumenda.
+              </p>
+              <br />
+              <p class="text-grey">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ipsa, incidunt impedit, facere
+                doloribus recusandae natus tenetur mollitia accusantium a quibusdam inventore? Ipsum numquam fugit culpa
+                alias suscipit. Praesentium, aperiam.
+              </p>
+              <v-btn tile dark color="yellow" class="mt-4">
+                Download Resume
+              </v-btn>
+            </v-col>
+          </v-row>
+        </div>
       </v-col>
+
+      <!-- portfolio -->
+      <div class="text-center mt-4">
+        <h2>What we Do</h2>
+        <div style="width: 120px; margin: 0 auto">
+          <v-slider v-model="slider2" color="yellow" track-color="yellow"></v-slider>
+        </div>
+      </div>
+      <v-col cols="12" class="padd" id="portfolio">
+        <div class="first" id="project">
+          <v-row>
+            <v-col cols="12">
+              <div class="child">
+                <v-btn icon="fas fa-laptop" color="#FBDF7E" class="text-white"></v-btn>
+                <h3 class="ml-3 mt-4">Web Design</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+
+              <div class="child">
+                <v-btn icon="fas fa-mobile-alt" color="#FBDF7E" class="text-white"></v-btn>
+                <h3 class="ml-3 mt-4">App Design</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+
+              <div class="child">
+                <v-btn icon="fas fa-camera" color="#FBDF7E" class="text-white"></v-btn>
+                <h3 class="ml-3 mt-4">Photoshop</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </v-col>
+
+      <!-- services -->
+      <v-col cols="12" sm="12" id="services">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mr-2">All</v-btn>
+          <v-btn class="mr-2" variant="tonal">Web Design</v-btn>
+          <v-btn class="mr-2" variant="tonal">Front Design</v-btn>
+          <v-btn class="mr-2" variant="tonal">Photography</v-btn>
+          <v-btn class="mr-2" variant="tonal">Illustration</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" class="imgHover">
+        <v-row class="fill-height" align="center" justify="center">
+          <template v-for="(item, i) in items" :key="i">
+            <v-col cols="12" md="4">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card :elevation="isHovering ? 12 : 2" :class="{ 'on-hover': isHovering }" v-bind="props">
+                  <v-img :src="item.img" height="225px" cover></v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+      <v-col cols="12" sm="12">
+        <v-row class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mt-4">Load More</v-btn>
+        </v-row>
+      </v-col>
+
+      <!-- page -->
+      <v-col cols="12" id="page">
+        <div class="pre">
+          <v-row>
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i10.jpg" height="200px" cover></v-img>
+
+                <v-card-title>We provide you the best</v-card-title>
+
+                <v-card-subtitle>
+                  By AAE IdeaPro | 06 sep 2022
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eaque dolorum, placeat hic harum
+                  suscipit.
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i11.jpg" height="200px" cover></v-img>
+
+                <v-card-title>We provide you the best</v-card-title>
+
+                <v-card-subtitle>
+                  By AAE IdeaPro | 06 sep 2022
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eaque dolorum, placeat hic harum
+                  suscipit.
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i12.jpg" height="200px" cover></v-img>
+
+                <v-card-title>We provide you the best</v-card-title>
+
+                <v-card-subtitle>
+                  By AAE IdeaPro | 06 sep 2022
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eaque dolorum, placeat hic harum
+                  suscipit.
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+
+      <!-- blog -->
+      <v-col cols="12" id="blog">
+        <div class="hire">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <h1 class="mt-9">Hire me for your awesome project</h1>
+              <p class="text-grey">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque cum voluptate
+                doloribus? Totam ducimus eius harum, obcaecati exercitationem at veniam officiis accusamus voluptas
+                quibusdam placeat voluptate reprehenderit recusandae, qui quisquam!
+              </p>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+
+      <!-- contact -->
+      <v-col cols="12" sm="12" class="px-16" id="contact">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <div class="child">
+              <h1>Contact info</h1>
+              <v-btn icon="fas fa-map-marker-alt" class="mt-10" variant="outlined"></v-btn><br />
+              <span class="text-caption">Your Street SYO, Your Area Dream</span><br />
+              <v-btn icon="fas fa-phone-alt" class="mt-10" variant="outlined"></v-btn><br />
+              <span class="text-caption">00235 -6547</span><br />
+              <span class="text-caption">00235 -6547</span><br />
+              <v-btn icon="fas fa-envelope" class="mt-10" variant="outlined"></v-btn><br />
+              <span class="text-caption">aaeideapro@gmail.com</span><br />
+              <span class="text-caption">aaeideapro@gmail.com</span><br />
+            </div>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h1 class="mt-8">Send your message</h1>
+            <v-divider></v-divider>
+            <span class="text-caption">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint tempora necessitatibus quaerat earum minima
+              voluptas harum reprehenderit facilis pariatur, explicabo voluptates asperiores modi eius nostrum quam?
+              Excepturi hic suscipit tempore.
+            </span>
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field label="Name" persistent-hint variant="outlined"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field label="Phone No" persistent-hint variant="outlined"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea label="Message" persistent-hint variant="outlined"></v-textarea>
+            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+
     </v-container>
+
+    <!-- footer -->
+    <FooterView />
   </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-
-// Components
 import NavBar from '@/components/NavBar.vue';
+import FooterView from '@/components/FooterView.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    NavBar
+    NavBar,
+    FooterView
   },
+
+  setup() {
+    return {
+      slider2: 50,
+
+      items: [
+        { img: "i3.jpg" },
+        { img: "i4.jpg" },
+        { img: "i5.jpg" },
+        { img: "i6.jpeg" },
+        { img: "i8.jpg" },
+        { img: "i9.jpg" }
+      ]
+    }
+  }
 });
 </script>
 
@@ -73,17 +308,62 @@ export default defineComponent({
   height: 100%;
   width: 50%;
   background: black;
-  transform: skew(0deg, 6deg);
+  transform: skew(0deg, 5deg);
 }
 
 .head:after {
   content: " ";
   position: absolute;
-  top: 0;
+  top: 0px;
   right: 0;
   height: 100%;
   width: 50%;
   background: black;
-  transform: skew(0deg, -6deg);
+  transform: skew(0deg, -5deg);
+}
+
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #FBDF7E;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+
+.first {
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: center;
+  margin-right: 8px;
+}
+
+.imgHover {
+  padding: 0 200px;
+}
+
+.pre {
+  width: 100%;
+  height: 380px;
+  text-align: center;
+  padding: 0 200px;
+  background-color: #f5f5f5;
+}
+
+.hire {
+  width: 100%;
+  height: 200px;
+  padding: 0 200px;
+  background-color: #e9e9e9;
+  margin-top: -24px;
 }
 </style>
